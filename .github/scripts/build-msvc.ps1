7b5afc60bde0b750e3bace10bd856248ab34240b
Jeff Harris <1063652+jeff-1amstudios@users.noreply.github.com>
2023-09-19 15:28:32 +1200

    windows test

--- .github/scripts/build-msvc.ps1
+++ .github/scripts/build-msvc.ps1
@@ -12,22 +12,27 @@ if ($($Env:GITHUB_REF_TYPE) -eq "tag") {
     $build_tag=$(git rev-parse --short HEAD)
 }
 
-$sdl2_version = "2.24.0"
-$sdl2_image_version = "2.6.3"
+$SDL2_VERSION = "2.24.0"
+$SDL2_IMAGE_VERSION = "2.6.3"
+$SDL2_TTF_VERSION = "2.20.2"
 
 # install deps
-Invoke-WebRequest -Uri https://www.libsdl.org/release/SDL2-devel-$sdl2_version-VC.zip -OutFile $Env:TEMP\SDL2-devel.zip
+Invoke-WebRequest -Uri https://www.libsdl.org/release/SDL2-devel-$SDL2_VERSION-VC.zip -OutFile $Env:TEMP\SDL2-devel.zip
 Expand-Archive $Env:TEMP\SDL2-devel.zip -DestinationPath $Env:TEMP
 
-Invoke-WebRequest -Uri https://github.com/libsdl-org/SDL_image/releases/download/release-2.6.3/SDL2_image-devel-2.6.3-VC.zip -OutFile $Env:TEMP\SDL2-image.zip
+Invoke-WebRequest -Uri https://github.com/libsdl-org/SDL_image/releases/download/release-$SDL2_IMAGE_VERSION/SDL2_image-devel-$SDL2_IMAGE_VERSION-VC.zip -OutFile $Env:TEMP\SDL2-image.zip
 Expand-Archive $Env:TEMP\SDL2-image.zip -DestinationPath $Env:TEMP
 
-$Env:SDL2_DIR = "$($Env:TEMP)\SDL2-$sdl2_version"
+Invoke-WebRequest -Uri https://github.com/libsdl-org/SDL_ttf/releases/download/release-$SDL2_TTF_VERSION/SDL2_ttf-devel-$SDL2_TTF_VERSION-VC.zip  -OutFile $Env:TEMP\SDL2-ttf.zip
+Expand-Archive $Env:TEMP\SDL2-ttf.zip -DestinationPath $Env:TEMP
+
+$Env:SDL2_DIR = "$($Env:TEMP)\SDL2-$SDL2_VERSION"
 
 # build
 cmake -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_TESTS=ON `
-  "-DSDL2_IMAGE_PATH=$($Env:TEMP)\SDL2_image-$sdl2_image_version" `
-  "-DSDL2_ROOT_DIR=$($Env:TEMP)\SDL2-$sdl2_version" `
+  "-DSDL2_IMAGE_PATH=$($Env:TEMP)\SDL2_image-$SDL2_IMAGE_VERSION" `
+  "-DSDL2_TTF_PATH=$($Env:TEMP)\SDL2_image-$SDL2_TTF_VERSION" `
+  "-DSDL2_ROOT_DIR=$($Env:TEMP)\SDL2-$SDL2_VERSION" `
   -B build
 
 if ($LASTEXITCODE -ne 0) {
@@ -39,13 +44,13 @@ if ($LASTEXITCODE -ne 0) {
 }
 
 # copy SDL2.dll to build folder, so tests can run
-cp $Env:TEMP\SDL2-$sdl2_version\lib\$sdl_path\SDL2.dll build
+cp $Env:TEMP\SDL2-$SDL2_VERSION\lib\$sdl_path\SDL2.dll build
 
 # package artifact
-$releasename="dethrace-$build_tag-windows-$Env:PLATFORM_ARCH"
+$releasename="skifree_sdl-$build_tag-windows-$Env:PLATFORM_ARCH"
 mkdir "$releasename"
-cp build/dethrace.exe "$releasename/dethrace.exe"
-cp build/dethrace.pdb "$releasename/dethrace.pdb"
+cp build/skifree_sdl.exe "$releasename/skifree_sdl.exe"
+cp build/skifree_sdl.pdb "$releasename/skifree_sdl.pdb"
 cp build/SDL2.dll "$releasename/SDL2.dll"
 
 7z a -tzip "$releasename.zip" "$releasename"
