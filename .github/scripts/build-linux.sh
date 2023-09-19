3ff0d57bbc68641bf837b1c2f6229fc74887f978
Jeff Harris <1063652+jeff-1amstudios@users.noreply.github.com>
2023-09-19 13:37:06 +1200

    adds pipeline

---
+++ .github/scripts/build-linux.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+if [ "${MATRIX_PLATFORM}" = "clang" ]
+then
+    export CC=clang
+    export CXX=clang++
+else
+    export CC=gcc
+    export CXX=g++
+fi
+
+if [[ "${GITHUB_REF_TYPE}" == "tag" ]]; then
+    BUILD_TAG=${GITHUB_REF_NAME}
+else 
+    BUILD_TAG=$(git rev-parse --short HEAD)
+fi
+
+# install deps
+sudo apt-get update -qq > /dev/null
+sudo apt-get install -qq -y libsdl2-dev > /dev/null
+
+# build
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_TESTS=ON -B build
+cmake --build build --
+
+# package artifact
+releasename="skifree_sdl-${BUILD_TAG}-linux-${PLATFORM_ARCH}"
+rm -rf "$releasename"
+mkdir "$releasename"
+cp build/skifree_sdl "$releasename/skifree_sdl"
+tar -czvf "$releasename.tar.gz" "$releasename"
+echo "filename=$releasename.tar.gz" >>$GITHUB_OUTPUT
+
---
+++ .github/scripts/build-macos.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+if [[ "${GITHUB_REF_TYPE}" == "tag" ]]; then
+    BUILD_TAG=${GITHUB_REF_NAME}
+else 
+    BUILD_TAG=$(git rev-parse --short HEAD)
+fi
+
+# install deps
+brew install SDL2 sdl2_image sdl2_ttf
+
+# build
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_OSX_ARCHITECTURES=x86_64 -B build
+cmake --build build --
+
+# package artifact
+releasename="skifree_sdl-${BUILD_TAG}-darwin-${PLATFORM_ARCH}"
+rm -rf "$releasename"
+mkdir "$releasename"
+cp build/skifree_sdl "$releasename/skifree_sdl"
+tar -czvf "$releasename.tar.gz" "$releasename"
+echo "filename=$releasename.tar.gz">>$GITHUB_OUTPUT
