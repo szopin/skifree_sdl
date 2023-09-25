41838b6da7c0f236b84dffb593a88da453f11198
Jeff Harris <1063652+jeff-1amstudios@users.noreply.github.com>
2023-09-25 20:58:36 +1300

    osx bundle has icon

---
+++ tools/make-osx-icns.sh
@@ -0,0 +1,12 @@
+mkdir ski32.iconset
+sips -z 16 16     ../resources/ICONSKI.png --out ski32.iconset/icon_16x16.png
+sips -z 32 32     ../resources/ICONSKI.png --out ski32.iconset/icon_16x16@2x.png
+sips -z 32 32     ../resources/ICONSKI.png --out ski32.iconset/icon_32x32.png
+sips -z 64 64     ../resources/ICONSKI.png --out ski32.iconset/icon_32x32@2x.png
+sips -z 128 128   ../resources/ICONSKI.png --out ski32.iconset/icon_128x128.png
+sips -z 256 256   ../resources/ICONSKI.png --out ski32.iconset/icon_128x128@2x.png
+sips -z 256 256   ../resources/ICONSKI.png --out ski32.iconset/icon_256x256.png
+sips -z 512 512   ../resources/ICONSKI.png --out ski32.iconset/icon_256x256@2x.png
+sips -z 512 512   ../resources/ICONSKI.png --out ski32.iconset/icon_512x512.png
+sips -z 1024 1024 ../resources/ICONSKI.png --out ski32.iconset/icon_512x512@2x.png
+iconutil -c icns ski32.iconset
