1d524f602217e75fe2ff85c968f0d91752cd0572
Jeff Harris <1063652+jeff-1amstudios@users.noreply.github.com>
2023-11-03 15:18:11 +1300

    osx framework

--- .github/scripts/build-macos.sh
+++ .github/scripts/build-macos.sh
@@ -15,16 +15,16 @@ fi
 # dont use brew as it installs sdl as .dylib not as a framework and breaks the .app package build
 #brew install SDL2 sdl2_image sdl2_ttf
 
-curl -o SDL2.dmg https://github.com/libsdl-org/SDL/releases/download/release-${SDL2_VERSION}/SDL2-${SDL2_VERSION}.dmg
+curl -Lo SDL2.dmg https://github.com/libsdl-org/SDL/releases/download/release-${SDL2_VERSION}/SDL2-${SDL2_VERSION}.dmg
 hdiutil attach SDL2.dmg -mountpoint /tmp/SDL2
 sudo cp -R /tmp/SDL2/SDL2.framework /Library/Frameworks/
 
 
-curl -o SDL2_image.dmg https://github.com/libsdl-org/SDL_image/releases/download/release-${SDL2_IMAGE_VERSION}/SDL2_image-${SDL2_IMAGE_VERSION}.dmg
+curl -Lo SDL2_image.dmg https://github.com/libsdl-org/SDL_image/releases/download/release-${SDL2_IMAGE_VERSION}/SDL2_image-${SDL2_IMAGE_VERSION}.dmg
 hdiutil attach SDL2_image.dmg -mountpoint /tmp/SDL2_image
 sudo cp -R /tmp/SDL2_image/SDL2_image.framework /Library/Frameworks/
 
-curl -o SDL2_ttf.dmg https://github.com/libsdl-org/SDL_ttf/releases/download/release-${SDL2_TTF_VERSION}/SDL2_ttf-${SDL2_TTF_VERSION}.dmg
+curl -Lo SDL2_ttf.dmg https://github.com/libsdl-org/SDL_ttf/releases/download/release-${SDL2_TTF_VERSION}/SDL2_ttf-${SDL2_TTF_VERSION}.dmg
 hdiutil attach SDL2_ttf.dmg -mountpoint /tmp/SDL2_ttf
 sudo cp -R /tmp/SDL2_ttf/SDL2_ttf.framework /Library/Frameworks/
 
