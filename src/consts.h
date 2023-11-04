dbd122e27aedaf3b4b507e01fd4e036648ea0b57
Jeff Harris <1063652+jeff-1amstudios@users.noreply.github.com>
2023-11-04 22:08:35 +1300

    tidy 2

---
+++ src/consts.h
@@ -0,0 +1,46 @@
+#ifndef SKIFREE_DECOMP_CONSTS_H
+#define SKIFREE_DECOMP_CONSTS_H
+
+#define NUM_ACTORS 100
+#define NUM_SPRITES 90
+#define NUM_STRINGS 20
+#define NUM_PERM_OBJECTS 256
+#define SMALL_TEXTURE_ATLAS_MAX_SIZE 32
+
+// Actor bit flags
+
+#define FLAG_1 1
+#define FLAG_2 2
+#define FLAG_4 4
+#define FLAG_8 8
+#define FLAG_10 0x10
+#define FLAG_20 0x20
+#define FLAG_40 0x40
+#define FLAG_80 0x80
+
+#define BORDER_LEFT 0
+#define BORDER_RIGHT 1
+#define BORDER_TOP 2
+#define BORDER_BOTTOM 3
+
+#define ACTOR_TYPE_0_PLAYER 0
+#define ACTOR_TYPE_1_BEGINNER 1
+#define ACTOR_TYPE_2_DOG 2
+#define ACTOR_TYPE_3_SNOWBOARDER 3
+#define ACTOR_TYPE_4_CHAIRLIFT 4
+#define ACTOR_TYPE_5_YETI_TOP 5
+#define ACTOR_TYPE_6_YETI_BOTTOM 6
+#define ACTOR_TYPE_7_YETI_LEFT 7
+#define ACTOR_TYPE_8_YETI_RIGHT 8
+#define ACTOR_TYPE_9_TREE_ON_FIRE 9
+#define ACTOR_TYPE_10_WALKING_TREE 10 // a
+#define ACTOR_TYPE_11_MOGULS 11       // b
+#define ACTOR_TYPE_12_SLALOM_FLAG 12  // c
+#define ACTOR_TYPE_13_TREE 13         // d
+#define ACTOR_TYPE_14_ROCK_STUMP 14   // e
+#define ACTOR_TYPE_15_BUMP 15         // f
+#define ACTOR_TYPE_16_JUMP 16         // 10
+#define ACTOR_TYPE_17_SIGN 17         // 11
+#define ACTOR_TYPE_18_NOTHING 18      // 12
+
+#endif
